298d8d49e09604173c22f8d6d48b4d5eb7c35042
webcapcl <168611642+webcapcl@users.noreply.github.com>
2026-04-29 21:39:41 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,128 @@
+# Atividade 1
+#print('teste') 
+
+# Atividade 2
+#print('teste2')
+
+# #while True:
+#     entrada = input("Digite apenas números: ")
+#     if entrada():
+#         numero = int(entrada)
+#         print(f"Número aceito: {numero}")
+#         break  # Sai do laço se for válido
+#     else:
+#         print("Digite apenas números.")
+
+
+#contador = 1
+# while contador <= 10:
+#     print(contador)
+
+#     if contador == 5:
+#         break
+#     contador +=1
+
+# #senha = ''
+# while True:
+#     senha = input("Digite a senha: ")
+#     if senha == '1234':
+#         numero = int(senha)
+#         print('Acesso liberado')
+#         break
+#     else:
+#         print('Senha incorreta ')
+
+# #numero = 1
+
+# while numero <= 5:
+#     if numero == 2:
+#         numero +=1
+#         continue
+#     print(numero)
+#     numero +=1
+
+
+ # ATIVIDADE 1
+
+numero = 1
+while numero <= 10:
+    print(numero)
+    numero += 1
+    if numero == 6:
+        break
+    
+
+# ATIVIDADE 2
+
+numero = 1 
+while numero <=10:
+    
+    if numero == 5:
+        numero += 1
+        continue
+    print(numero)
+    numero +=1
+
+# ATIVIDADE 3
+
+numero = 1
+while numero <= 20:
+
+    if numero == 17:
+        break
+    if numero % 2 == 0:
+        numero +=1
+        continue
+    print(numero)
+    numero +=1    
+
+# Atividade 4
+    produto = ''
+    while True:
+        produto = input('Digite o produto ')
+        if produto == 'fim':
+            print('cadastro finalizado ')
+            break
+        print('Produto cadastrado', produto)   
+
+
+
+# Atividade 5
+
+soma = 0 
+while True:
+    numero = int(input('Digite um número '))
+    soma += numero
+    if soma >= 20:
+      break
+print('Total', soma)
+
+# Atividade 6
+
+soma = 0 
+while True:
+    numero = int(input('Digite um número '))
+    soma += numero
+    if soma > 50:
+     print('Limite atingido ')    
+     print('Total: ',soma)    
+     break
+
+
+
+
+   
+
+
+
+
+
+    
+
+
+
+
+
+
+
+        
